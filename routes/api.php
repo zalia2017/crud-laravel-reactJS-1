ff6558c83fc2b8c06fc425334ca4bd77561cd2f5
zalia2017 <zul_kali@yahoo.com>
2020-09-26 18:44:23 +0700

    Initial awal

---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::get('/articles', 'ArticleController@index');
+// Route::post('/articles/store', 'ArticleController@store');
+// Route::get('/article/edit/{id}', 'ArticleController@getArticle');
+// Route::get('/article/{id}', 'ArticleController@getArticle');
+// Route::put('/article/{id}', 'ArticleController@update');
+// Route::delete('/article/delete/{id}', 'ArticleController@delete');
+
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::post('/articles/store', [ArticleController::class, 'store']);
+Route::get('/article/edit/{id}', [ArticleController::class, 'getArticle']);
+Route::get('/article/{id}', [ArticleController::class, 'getArticle']);
+Route::put('/article/{id}', [ArticleController::class, 'update']);
+Route::delete('/article/delete/{id}', [ArticleController::class, 'delete']);
